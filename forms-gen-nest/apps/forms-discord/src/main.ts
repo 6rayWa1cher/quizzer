d2c94a0d0c242602d34c99249e0c15f315b579fd
Alstrasz <lutsai2000@ya.ru>
2022-08-30 17:26:08 +0300

    Filled forms-discord service

--- forms-gen-nest/apps/forms-discord/src/main.ts
+++ forms-gen-nest/apps/forms-discord/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { FormsDiscordModule } from './forms-discord.module';
 
 async function bootstrap () {
-    const app = await NestFactory.create( FormsDiscordModule );
+    const app = await NestFactory.create(
+        FormsDiscordModule,
+        {
+            logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+        },
+    );
     await app.listen( 3000 );
 }
 bootstrap();
